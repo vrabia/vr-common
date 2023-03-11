e990739772fb28fe7cbb11a752867c9ec20823a3
Arcalean Maria Lorena <arcalean.lorena22@gmail.com>
2023-03-11 22:04:29 +0200

    first commit

---
+++ src/test/java/app/vrabia/vrcommon/VrCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vrabia.vrcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
